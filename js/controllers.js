ee527a9d18f52e83514e0241946bec183b465955
ggitau <ggitau@ilabafrica.co.ke>
2013-12-06 13:21:12 +0300

    Deleting the last item no longer results in the deletion of the second last item

--- js/controllers.js
+++ js/controllers.js
@@ -1,14 +1,17 @@
 function TodoCtrl($scope){
 	$scope.currentTask={};
-	$scope.tasks=[];
+	$scope.tasks={};
+    $scope.currentIndex=0;
 	$scope.addTask = function(){
-		$scope.tasks.push({description:$scope.currentTask.description,status:'not_done'});
+		$scope.tasks[$scope.currentIndex++]={description:$scope.currentTask.description,status:'not_done'};
 		$scope.currentTask={};
 	};
 	$scope.markDone=function(i){
 		$scope.tasks[i].style="text-decoration:line-through;";
 	};
+
 	$scope.deleteTask = function(i){
-		$scope.tasks.splice(i-1,1);
+        delete $scope.tasks[i];
+        $scope.currentIndex--;
 	}
 }
--- js/todo.js
+++ js/todo.js
@@ -14,3 +14,28 @@ todo.directive('ngEnter', function () {
         });
     };
 });
+todo.directive('taskRowContent',function(){
+    return {
+        restrict:'E',
+        scope:{
+            index:'@',
+            style:'@',
+            task:'@',
+            deleteTask:'&',
+            markDone:'&'
+
+        },template:'<span><span class="span10" style="{{style}}">{{index}}.{{task}}</span>'
+					+'<span class="span2"><span title="delete task" style="cursor:pointer;" ng-click="deleteTask({{index-1}});"><i class="icon-remove"></i></span>'
+					+'<span title="mark task as done" style="cursor:pointer;" ng-click="markDone(\'{{index-1}}\');"><i class="icon-ok"></i></span>'
+					+'</span></span>'
+        ,replace:true            
+        ,link:function(scope,element,attrs){
+            scope.deleteTask=function(index){
+                scope.$parent.deleteTask(index);
+            };
+            scope.markDone=function(index){
+                scope.$parent.markDone(index);
+            }
+        }
+    };
+});
